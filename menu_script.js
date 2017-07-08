b97179131f1d50997c0bf15ff26e0cb4eea49aa4
FeHioe <fe.hioe@gmail.com>
2017-07-07 20:59:51 -0400

    Header, Menu and About update
    Todo:
    -Portfolio layout
    -Contact form
    -Fix CSS bug in portfolio

--- menu_script.js
+++ menu_script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   /* Force To Top on Refresh */
   window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-    $("#logo").hide();
   }
 
-  /* Get View Height */
-  var height = document.getElementById('header_height').clientHeight;
+  /* Get Heights */
+  var height = document.getElementById('header_height').clientHeight - 75;
+  var about_height = document.getElementById("quarter").clientHeight;
 
   /* Fade In Header Animations */
   $("#tagline h3").addClass("load");
@@ -20,16 +20,30 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop: height + 1}, 'slow');
   });
 
+  $("#logo").on("click", function() {
+    $('html, body').animate({scrollTop: 0}, 'slow');
+  })
+
+  $("#about_link").on("click", function() {
+    $('html, body').animate({scrollTop: height + 1}, 'slow');
+  });
+
+  $("#portfolio_link").on("click", function() {
+    $('html, body').animate({scrollTop: height + about_height}, 'slow');
+  });
+
+  $("#contact_link").on("click", function() {
+    $('html, body').animate({scrollTop: document.body.scrollHeight}, 'slow');
+  });
+
   /* Sticky Menu */
   $(window).scroll(function () {
     console.log($(window).scrollTop())
 
     if ($(window).scrollTop() > height) {
-      $('#menu').addClass('menu_sticky');
-      $('#logo').fadeIn(500);
+      $('#menu').fadeIn(500);
     } else if ($(window).scrollTop() < height + 1) {
-      $('#menu').removeClass('menu_sticky');
-      $('#logo').fadeOut(500);
+      $('#menu').fadeOut(500);
     }
 
   });
