ae4a57a45fd63dd8ec909d8d6680f4448a35c595
FeHioe <fe.hioe@gmail.com>
2025-10-02 19:36:15 -0400

    add basic info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="name-card">
+        <h1>FELICIA HIOE</h1>
+        <p> Full Stack Developer</p>
+        <p> fe.hioe@gmail.com</p>
+        <p> Toronto, Ontario, Canada</p>
     </div>
   );
 }
