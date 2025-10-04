6d890f5442fc0e440736a2ab04e3899b5ff96ea4
FeHioe <fe.hioe@gmail.com>
2025-10-03 21:55:43 -0400

    homepage base

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="name-card">
-        <h1>FELICIA HIOE</h1>
-        <p> Full Stack Developer</p>
-        <p> fe.hioe@gmail.com</p>
-        <p> Toronto, Ontario, Canada</p>
+    <div className="App">
+      <div className="name-card">
+          <h1>FELICIA HIOE</h1>
+          <div className="info">        
+            <p> Full Stack Developer</p>
+            &bull;
+            <p> fe.hioe@gmail.com</p>
+            &bull;
+            <p> Toronto, Ontario, Canada</p>
+          </div>
+      </div>
+      <div className="menu">
+        <div className="bullet"><a>&#11044; Resume</a></div>
+        <div className="bullet"><a>&#11044; Github</a></div>
+        <div className="bullet"><a>&#11044; Projects</a></div>
+        <div className="bullet"><a>&#11044; About</a></div>
+      </div>
     </div>
   );
 }
